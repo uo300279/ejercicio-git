4dcbc7b7e63dd56ba7c32dc4c9ccff74ec6b9180
Hugo Fernández López <uo300279>
2026-02-08 13:06:49 +0100

    Fijando master

--- src/Demo/Main.java
+++ src/Demo/Main.java
@@ -3,7 +3,7 @@ package Demo;
 public class Main {
 	// este es un cambio directo en master
 	public static void main(String[] args) {
-		System.out.println("Linea cambiada por la rama");
+		System.out.println("MASTER quiere esta linea");
     }
 
 }
