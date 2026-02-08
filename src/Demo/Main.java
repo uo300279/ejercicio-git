550dcce57572b3b06b67a13587ad1f73c7c9e172
Hugo Fernández López <uo300279>
2026-02-08 13:08:15 +0100

    Master cambia de nuevo

--- src/Demo/Main.java
+++ src/Demo/Main.java
@@ -3,7 +3,7 @@ package Demo;
 public class Main {
 	// este es un cambio directo en master
 	public static void main(String[] args) {
-		System.out.println("MASTER quiere esta linea");
+		System.out.println("MASTER ha cambiado de idea");
     }
 
 }
